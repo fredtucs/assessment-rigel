b36153a983e447a45ff6f895a5a6c2a651452c5d
Fredy Tuco <fredtucs7@gmail.com>
2023-04-07 15:51:15 -0500

    clase: mejora de control asistencia

--- EvaluacionClase/Repository.js
+++ EvaluacionClase/Repository.js
@@ -1,10 +1,17 @@
-var scriptProp = PropertiesService.getScriptProperties();
+let scriptProp = PropertiesService.getScriptProperties();
 
 function loadDataEvaluacion() {
   const ss = SpreadsheetApp.openById(scriptProp.getProperty('key'));
-  let data = ss.getRangeByName('EvalClase').getValues();
-  data = data.map(el => el.filter(ex => ex != ''));
-  return data;
+  const data = ss.getRangeByName('EvalClase').getValues();
+  const result = data.map(arr => {
+    const [title, ...items] = arr.filter(Boolean);
+    const [criteItems, valueItems] = items.reduce(([criteAcc, valueAcc], item) => {
+      isNaN(item) ? criteAcc.push(item) : valueAcc.push(item);
+      return [criteAcc, valueAcc];
+    }, [[], []]);
+    return [title, criteItems, valueItems];
+  });
+  return result;
 }
 
 function loadData(nameData) {
@@ -45,8 +52,10 @@ function saveMainData(formData) {
 }
 
 function saveAttendanceData(formData) {
+  const justInasistencia = formData.justInasistencia ? formData.justInasistencia : '';
   const doc = SpreadsheetApp.openById(scriptProp.getProperty('key'));
   const sheet = doc.getSheetByName(formData.clase);
+  const now = new Date();  
   sheet.appendRow([
     formData.fecha,
     formData.evalPerson,
@@ -55,16 +64,17 @@ function saveAttendanceData(formData) {
     formData.evalDisciplina,
     formData.evalResponsabilidad,
     formData.evalParticipacion,
-    new Date()
+    justInasistencia,
+    now
   ]);
 }
 
 function horarioClases() {
-  var today = new Date();
-  var weekday = today.getDay();
-  var hourOpen = new Date();
+  let today = new Date();
+  let weekday = today.getDay();
+  let hourOpen = new Date();
   hourOpen.setHours(10, 0, 0);
-  var hourClose = new Date();
+  let hourClose = new Date();
   hourClose.setHours(11, 0, 0);
-  return (weekday == 0) && (today >= hourOpen && today < hourClose);
+  return true//(weekday == 0) && (today >= hourOpen && today < hourClose);
 }
